202ed0d8dfa681f81d58453e92bf941c43d90f27
DawidM <dawid.dutkiewicz@interia.pl>
2018-12-07 01:50:04 +0100

    Added functionality that allows player and computer to place a tanks

--- app.js
+++ app.js
@@ -4,7 +4,82 @@
 
 ******************************/
 const tanksController = (function() {
+  let Tank = function(id, x, y) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+  };
 
+  let boardData = {
+    playerBoard: [],
+    computerBoard: []
+  };
+
+  return {
+    test: function() {
+      return boardData;
+    },
+
+    
+    getBoardData: function() {
+      return boardData;
+    },
+
+
+    setPlayerTank: function(id, x, y) {
+      let tank;
+
+      tank = new Tank(id, x, y);
+
+      boardData.playerBoard.push(tank);
+    },
+
+    fillEnemyBoard: function() {
+      let x, y, placedX, placedY, tank, validated, id;
+      x = Math.floor(Math.random() * 10);
+      y = Math.floor(Math.random() * 10);  
+
+      tank = new Tank(1, x, y);
+
+      boardData.computerBoard.push(tank);
+     
+      do {
+        validated = true;
+        x = Math.floor(Math.random() * 10);
+        y = Math.floor(Math.random() * 10);  
+        for(let i = 0; i < boardData.computerBoard.length; i++) {
+          placedX = boardData.computerBoard[i].x;
+          placedY = boardData.computerBoard[i].y;
+  
+          if(placedX === x && placedY === y) {
+            validated = false;
+          } 
+
+          if(x >= 0  && x < 10 && y >= 0 && y < 10) {
+            if(  ( placedX + 1 === x && placedY === y )
+              || ( placedX - 1 === x && placedY === y )
+              || ( placedY + 1 === y && placedX === x ) 
+              || ( placedY - 1 === y && placedX === x )
+              || ( placedX + 1 === x && placedY + 1 === y )
+              || ( placedX - 1 === x && placedY + 1 === y )
+              || ( placedX - 1 === x && placedY - 1 === y )
+              || ( placedX + 1 === x && placedY - 1 === y )
+            ) validated = false;
+          }
+          id = i + 2;
+        }
+
+        if(validated) {
+          tank = new Tank(id, x, y);
+          boardData.computerBoard.push(tank);
+        }
+        
+      } while (boardData.computerBoard.length <= 6);
+    },
+    // setComputerTanks: function(x, y) {
+
+    // }
+  }
 })();
 
 
@@ -15,11 +90,26 @@ const tanksController = (function() {
 ******************************/
 const UIController = (function() {
   let DOMstrings = {
+    gameMenu: '.game-menu',
+    game: '.game',
     playerBoard: '#player-board',
-    computerBoard: '#computer-board'
+    computerBoard: '#computer-board',
+    gameStartBtn: '#game-start-btn',
+    container: '.container',
+    gameSettings: '.game__settings-board',
+    boardElement: '.board-element'
   }
 
   return {
+    getDOMelemenets: function() {
+      return DOMstrings;
+    },
+
+    showPlayerSettingBoard: function() {
+      document.querySelector(DOMstrings.gameMenu).classList.add('hidden');
+      document.querySelector(DOMstrings.game).classList.remove('hidden');
+    },
+
     createBoards: function() {
       let html, element;
 
@@ -45,6 +135,21 @@ const UIController = (function() {
           element.insertAdjacentHTML('beforeend', newHtml);
         }
       }
+    },
+
+    placeTankInUI: function(item) {
+      document.querySelector('#'+item).style.backgroundImage = "url('Tank.png')";
+    },
+
+    showEnemyBoard: function() {
+      let containers;
+      containers = document.querySelectorAll(DOMstrings.container);
+
+      containers[1].classList.toggle('hidden');
+
+      document.querySelector(DOMstrings.gameSettings).classList.toggle('hidden');
+      document.querySelector(DOMstrings.playerBoard).id = "player-board-complete";
+      document.querySelector(DOMstrings.computerBoard).classList.toggle('enemy-board-ready');
     }
   };
 })();
@@ -56,13 +161,90 @@ const UIController = (function() {
 ******************************/
 const controller = (function(tanksCtrl, UICtrl) {
   let setupEventListeners = function() {
+    const DOM = UICtrl.getDOMelemenets();
+    document.querySelector(DOM.gameStartBtn).addEventListener('click', startGame);
 
-  }
+    document.querySelector(DOM.playerBoard).addEventListener('click', placeTanks);
+  };
+
+  let putTank = function(e, boardData) {
+    let item, dividedItem, id, x, y, validated = true, placedX, placedY;
+
+    item = e.target.id;
+      dividedItem = item.split('-');
+      x = parseInt(dividedItem[1]);
+      y = parseInt(dividedItem[2]);
+        
+      if(boardData.playerBoard.length !== 0) {
+        for(let i = 0; i < boardData.playerBoard.length; i++) {
+          placedX = boardData.playerBoard[i].x;
+          placedY = boardData.playerBoard[i].y;
+          
+          if(placedX === x && placedY === y) {
+            validated = false;
+            break;
+          } 
+              // 2, Sprawdz czy czolg nie jest umieszczany za blisko istniejacego czolgu
+          if(x >= 0  && x < 10 && y >= 0 && y < 10) {
+            if(  ( placedX + 1 === x && placedY === y )
+              || ( placedX - 1 === x && placedY === y )
+              || ( placedY + 1 === y && placedX === x ) 
+              || ( placedY - 1 === y && placedX === x )
+              || ( placedX + 1 === x && placedY + 1 === y )
+              || ( placedX - 1 === x && placedY + 1 === y )
+              || ( placedX - 1 === x && placedY - 1 === y )
+              || ( placedX + 1 === x && placedY - 1 === y )
+            ) validated = false;
+          } 
+        }    
+      }
+      
+      if(typeof placedX === "undefined") placedX = 0;
+      if(typeof placedY === "undefined") placedY = 0;
+
+      if(!isNaN(x) && !isNaN(y) && !isNaN(placedX) && !isNaN(placedY)) {
+        if(validated) {
+          tanksCtrl.setPlayerTank(1, x, y);
+          UICtrl.placeTankInUI(item);
+        }
+      }
+  };
+
+  let placeTanks = function(e) {
+    let boardData, tankCounter;
+
+    // Pobierz informacje o planszy gracza
+    boardData = tanksCtrl.getBoardData();
+      
+    tankCounter = boardData.playerBoard.length;
+    if(tankCounter <= 7) {
+      putTank(e, boardData);
+      tankCounter = boardData.playerBoard.length;
+      if(tankCounter === 7) {
+        UICtrl.showEnemyBoard();
+      }
+    }
+  };
+
+  let startGame = function() {
+    // 1. Wygeneruj plansze i pokaz je
+    UICtrl.createBoards();
+    UICtrl.showPlayerSettingBoard();
+    tanksCtrl.fillEnemyBoard();
+
+    // // 2. Popros gracza o ustawienie czolgów
+    // tanksCtrl.setPlayerTanks();
+
+    // // 3. Ustaw czolgi przeciwnika 
+    // tanksCtrl.setComputerTanks();
+
+    // // 4. Pokaz obie planszei ukryj instrukcje
+    // UICtrl.showEnemyBoard();
+  };
 
   return {
     init: function() {
       setupEventListeners();
-      UICtrl.createBoards();
     }
   };
 })(tanksController, UIController);
